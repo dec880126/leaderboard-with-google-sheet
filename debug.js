66e7cf362397d3d7bdde146ab1bf69c71833b8b6
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-08-13 03:26:29 +0800

    Create debug.js

---
+++ debug.js
@@ -0,0 +1,12 @@
+function debug() {
+  doGet(
+    {
+      "parameter":{
+        startRow:2,
+        startColumn:3,
+        url: "https://docs.google.com/spreadsheets/d/1VkBUIGg9duRCCH2rfuuhzcsxsW-2OES-xzZL6BRGyio/edit#gid=293141823",
+        name: "answerSheet"
+      }
+    }
+  );
+}
